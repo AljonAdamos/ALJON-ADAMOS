05f0b364726ede531f49f1413cb44bd6149c701c
aljon adamos <adamosaljon63@gmail.com>
2022-12-28 23:51:28 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+//script.js
+//email section
+function SendMail() {
+    var params = {
+        from_name : document.getElementById("fullname").value,
+        email_id : document.getElementById("email_id").value,
+        message : document.getElementById("message").value,
+    }
+    emailjs.send("service_yfgscy9", "template_qldeo4d", params).then(function (res){
+        alert("Successfully Sent! " + res.status);
+    })
+}
