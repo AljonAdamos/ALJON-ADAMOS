d18b5b0efac847ddf678de8f7143b2b9b6dd8717
aljon adamos <adamosaljon63@gmail.com>
2022-12-30 13:49:04 +0800

    added changes to code

--- js/script.js
+++ js/script.js
@@ -16,14 +16,9 @@ function SendMail() {
     var params = {
         from_name : document.getElementById("fullname").value,
         email_id : document.getElementById("email_id").value,
-        message : document.getElementById("message").value,
+        message : document.getElementById("message").value
     }
-    emailjs.send("service_yfgscy9", "template_qldeo4d", params).then(function (res){
+    emailjs.send("service_gvepd5c", "template_3v1duct", params).then(function (res) {
         alert("Successfully Sent! " + res.status);
-    });
-    
-    // reset form here, if needed
-    reset();
-
-    return false;
+    })
 }
